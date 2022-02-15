74ad8d0a9480fa5be853c6a81b909e180ae812eb
Alexander Melnik <melnsanya@gmail.com>
2022-02-15 20:14:36 +0200

    update_2

--- app/src/test/java/education/part1/task1/SolutionTest.java
+++ app/src/test/java/education/part1/task1/SolutionTest.java
@@ -5,15 +5,29 @@ import static org.junit.jupiter.api.Assertions.*;
 class SolutionTest {
 
     @org.junit.jupiter.api.Test
-    void testIsYearLeap() {
-        int[] expectedYears = {-392, 144, 2012, 2000, 1460};
+    void testIsYearLeapAD() {
+        int[] expectedYears = {392, 144, 2012, 2000, 1460};
         for(int expectedYear : expectedYears) {
             assertTrue(Solution.isYearLeap(expectedYear));
         }
     }
     @org.junit.jupiter.api.Test
-    void testIsYearNotLeap() {
-        int[] expectedYears = {-9, 150, 600, 1800, 1446};
+    void testIsYearNotLeapAD() {
+        int[] expectedYears = {9, 150, 600, 1800, 1446};
+        for(int expectedYear : expectedYears) {
+            assertFalse(Solution.isYearLeap(expectedYear));
+        }
+    }
+    @org.junit.jupiter.api.Test
+    void testIsYearLeapBC() {
+        int[] expectedYears = {-392, -144, -2012, -2000, -1460};
+        for(int expectedYear : expectedYears) {
+            assertTrue(Solution.isYearLeap(expectedYear));
+        }
+    }
+    @org.junit.jupiter.api.Test
+    void testIsYearNotLeapBC() {
+        int[] expectedYears = {-9, -150, -600, -1800, -1446};
         for(int expectedYear : expectedYears) {
             assertFalse(Solution.isYearLeap(expectedYear));
         }
